b9036154848950ecec2ab095566e4c9590970150
Suriya Ganesh <suriyaganesh097@gmail.com>
2021-03-15 22:29:49 +0530

    updated signup and login

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 from .views import index
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index),
-    path('home/', views.index),
-    path('list/', views.index),
+
+    path('', views.index, name='home'),
+    path('sign_up/', views.sign_up, name="sign-up"),
     path('people/', views.index),
+    path('accounts/', include('django.contrib.auth.urls'))
 
 ]
